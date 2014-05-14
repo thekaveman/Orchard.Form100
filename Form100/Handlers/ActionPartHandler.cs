b04951cb9971d7242b78a29693c4cd7598fb340f
Kegan Maher <kegan.maher@smgov.net>
2014-05-14 15:16:20 -0700

    add storage filters for the Action and Employee parts

---
+++ Form100/Handlers/ActionPartHandler.cs
@@ -0,0 +1,16 @@
+﻿using Orchard.ContentManagement.Handlers;
+using Orchard.Data;
+
+namespace CSM.Form100.Handlers
+{
+    using Models;
+
+    public class ActionPartHandler : ContentHandler
+    {
+        public ActionPartHandler(IRepository<ActionPartRecord> repository)
+        {
+            // *conventional* way to configure persistence for *PartRecord backing classes
+            Filters.Add(StorageFilter.For(repository));
+        }
+    }
+}
---
+++ Form100/Handlers/EmployeePartHandler.cs
@@ -0,0 +1,16 @@
+﻿using Orchard.ContentManagement.Handlers;
+using Orchard.Data;
+
+namespace CSM.Form100.Handlers
+{
+    using Models;
+
+    public class EmployeePartHandler : ContentHandler
+    {
+        public EmployeePartHandler(IRepository<EmployeePartRecord> repository)
+        {
+            // *conventional* way to configure persistence for *PartRecord backing classes
+            Filters.Add(StorageFilter.For(repository));
+        }
+    }
+}
