85762134baf2a86f821e0b473838a238f7efdf23
Kegan Maher <kegan.maher@smgov.net>
2014-05-14 15:15:52 -0700

    a basic service to get review approvals

---
+++ Form100/Services/IReviewApprovalService.cs
@@ -0,0 +1,11 @@
+﻿using Orchard;
+
+namespace CSM.Form100.Services
+{
+    using Models;
+
+    public interface IReviewApprovalService : IDependency
+    {
+        ReviewApprovalRecord Get(int id);
+    }
+}
