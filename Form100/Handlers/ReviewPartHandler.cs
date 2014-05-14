892b43a8c50a6db7e787035894dcdc0262f7bb9c
Kegan Maher <kegan.maher@smgov.net>
2014-05-14 15:16:53 -0700

    add storage filter and setup lazy field on ReviewPart

---
+++ Form100/Handlers/ReviewPartHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using Orchard.ContentManagement.Handlers;
+using Orchard.Data;
+
+namespace CSM.Form100.Handlers
+{
+    using Models;
+    using Services;
+
+    public class ReviewPartHandler : ContentHandler
+    {
+        public ReviewPartHandler(IRepository<ReviewPartRecord> repository, IReviewApprovalService reviewApprovalService)
+        {
+            // *conventional* way to configure persistence for *PartRecord backing classes
+            Filters.Add(StorageFilter.For(repository));
+
+            OnActivated<ReviewPart>((context, reviewPart) => {
+
+                reviewPart.ApprovalsField.Loader(_ => {
+                    return reviewPart.Record.ApprovalIds.Any()
+                         ? reviewPart.Record.ApprovalIds.Select(id => reviewApprovalService.Get(id)).ToList()
+                         : new List<ReviewApprovalRecord>();
+                });
+
+                reviewPart.ApprovalsField.Setter(approvals => {
+                    reviewPart.Record.ApprovalIds = approvals != null && approvals.Any()
+                        ? approvals.Select(a => a.Id).ToList()
+                        : new List<int>();
+                    return approvals;
+                });
+            });
+        }
+    }
+}
