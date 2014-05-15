0932ea7c51a0f5bed2321af2e479f068a3f9a6b2
Kegan Maher <kegan.maher@smgov.net>
2014-05-14 20:25:00 -0700

    ReviewPart exporting

--- Form100/Drivers/ReviewPartDriver.cs
+++ Form100/Drivers/ReviewPartDriver.cs
@@ -1,4 +1,9 @@
-﻿using Orchard.ContentManagement.Drivers;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using Orchard.ContentManagement.Drivers;
+using Orchard.ContentManagement.Handlers;
 
 namespace CSM.Form100.Drivers
 {
@@ -7,6 +12,8 @@ namespace CSM.Form100.Drivers
 
     public class ReviewPartDriver : ContentPartDriver<ReviewPart>
     {
+        private readonly string dateFormat = "yyyy-dd-MM";
+
         private readonly IReviewApprovalService reviewApprovalService;
 
         public ReviewPartDriver(IReviewApprovalService reviewApprovalService)
@@ -31,6 +38,34 @@ namespace CSM.Form100.Drivers
                     () => shapeHelper.Parts_Review_Status(Status: part.Status)
                 )
             );
+        }
+
+        protected override void Exporting(ReviewPart part, ExportContentContext context)
+        {
+            var reviewNode = context.Element(part.PartDefinition.Name);
+
+            reviewNode.SetAttributeValue("Status", part.Status);
+
+            if (part.ApprovalChain.Any())
+            {
+                var copy = new Queue<ReviewApprovalRecord>(part.ApprovalChain);
+                var approvalChainNode = new XElement("ApprovalChain");
+
+                while (copy.Any())
+                {
+                    var approvalRecord = copy.Dequeue();
+                    var approvalRecordNode = new XElement("ApprovalRecord");
+                    
+                    approvalRecordNode.SetAttributeValue("Id", approvalRecord.Id);
+                    approvalRecordNode.SetAttributeValue("IsApproved", approvalRecord.IsApproved);
+                    approvalRecordNode.SetAttributeValue("ApprovalDate", approvalRecord.ApprovalDate.HasValue ? approvalRecord.ApprovalDate.Value.ToString(dateFormat) : String.Empty);
+                    approvalRecordNode.SetAttributeValue("ApproverName", approvalRecord.ApproverName);
+
+                    approvalChainNode.Add(approvalRecordNode);
+                }
+
+                reviewNode.Add(approvalChainNode);
+            }
         }
     }
 }
