7a01dca635543479488807768cd0293fe1072264
Kegan Maher <kegan.maher@smgov.net>
2014-05-14 22:34:25 -0700

    implement exporting for EmployeePart

--- Form100/Drivers/EmployeePartDriver.cs
+++ Form100/Drivers/EmployeePartDriver.cs
@@ -1,4 +1,6 @@
-﻿using Orchard.ContentManagement.Drivers;
+﻿using System.Xml.Linq;
+using Orchard.ContentManagement.Drivers;
+using Orchard.ContentManagement.Handlers;
 
 namespace CSM.Form100.Drivers
 {
@@ -6,11 +8,17 @@ namespace CSM.Form100.Drivers
 
     public class EmployeePartDriver : ContentPartDriver<EmployeePart>
     {
+        private readonly string dateFormat = "yyyy-dd-MM";
+
         protected override string Prefix
         {
             get { return "Employee"; }
         }
 
+        /// <summary>
+        /// Respond to requests to display data from this part
+        /// e.g. return a bunch of so called "display" shapes
+        /// </summary>
         protected override DriverResult Display(EmployeePart part, string displayType, dynamic shapeHelper)
         {
             return Combined(
@@ -23,14 +31,57 @@ namespace CSM.Form100.Drivers
                     () => shapeHelper.Parts_Employee_Name(FirstName: part.FirstName, LastName: part.LastName)
                 ),
                 ContentShape(
-                    "Parts_Employee_PriorJobClass",
-                    () => shapeHelper.Parts_Employee_PriorJobClass(JobClass: part.PriorJobClass)
+                    "Parts_Employee_PriorJobStep",
+                    () => shapeHelper.Parts_Employee_PriorJobStep(JobStep: part.PriorJobStep)
                 ),
                 ContentShape(
-                    "Parts_Employee_CurrentJobClass",
-                    () => shapeHelper.Parts_Employee_CurrentJobClass(JobClass: part.CurrentJobClass)
+                    "Parts_Employee_CurrentJobStep",
+                    () => shapeHelper.Parts_Employee_CurrentJobStep(JobStep: part.CurrentJobStep)
                 )                
             );
+        }
+
+        /// <summary>
+        /// Define how EmployeePart data is exported.
+        /// Hint: it uses XML.
+        /// </summary>
+        protected override void Exporting(EmployeePart part, ExportContentContext context)
+        {
+            var employeeNode = context.Element(part.PartDefinition.Name);
+
+            employeeNode.SetAttributeValue("EmployeeId", part.EmployeeId);
+            employeeNode.SetAttributeValue("FirstName", part.FirstName);
+            employeeNode.SetAttributeValue("LastName", part.LastName);
+
+            if (part.PriorJobStep != null)
+            {
+                var jobStepNode = createJobStepNode("PriorJobStep", part.PriorJobStep);
+
+                employeeNode.Add(jobStepNode);
+            }
+
+            if (part.CurrentJobStep != null)
+            {
+                var jobStepNode = createJobStepNode("CurrentJobStep", part.CurrentJobStep);
+
+                employeeNode.Add(jobStepNode);
+            }
+        }
+
+        private static XElement createJobStepNode(string name, JobStepRecord jobStep)
+        {
+            var jobStepNode = new XElement(name);
+            
+            jobStepNode.SetAttributeValue("Id", jobStep.Id);
+            jobStepNode.SetAttributeValue("Title", jobStep.Title);
+            jobStepNode.SetAttributeValue("DepartmentName", jobStep.DepartmentName);
+            jobStepNode.SetAttributeValue("DivisionName", jobStep.DivisionName);
+            jobStepNode.SetAttributeValue("DivisionNumber", jobStep.DivisionNumber);
+            jobStepNode.SetAttributeValue("StepNumber", jobStep.StepNumber);
+            jobStepNode.SetAttributeValue("Id", jobStep.Id);
+            jobStepNode.SetAttributeValue("HoursPerWeek", jobStep.HoursPerWeek);
+
+            return jobStepNode;
         }
     }
 }
