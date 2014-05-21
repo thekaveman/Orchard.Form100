2337ebfc6d52c19a3641b96409182401661ad052
Kegan Maher <kegan.maher@smgov.net>
2014-05-20 17:50:05 -0700

    set default value for WorkflowStatus to New

--- Form100/Models/ReviewPartRecord.cs
+++ Form100/Models/ReviewPartRecord.cs
@@ -1,11 +1,18 @@
 ﻿using Orchard.ContentManagement.Records;
+using Orchard.Data.Conventions;
 
 namespace CSM.Form100.Models
 {
     public class ReviewPartRecord : ContentPartRecord
     {
+        [StringLengthMax]
         public virtual string ApprovalChainIds { get; set; }
 
         public virtual WorkflowStatus Status { get; set; }
+
+        public ReviewPartRecord()
+        {
+            Status = WorkflowStatus.New;
+        }
     }
 }
